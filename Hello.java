7616fc7b91880b7cd85ece3d40eaf247be4004dd
azamatpro <azamat.rasulov00@gmail.com>
2024-05-19 09:32:30 +0900

    try with resources to call close() automatically

--- Hello.java
+++ Hello.java
@@ -4,14 +4,9 @@ import java.util.Scanner;
 public class Hello {
   public static void main(String args[]) throws IOException{
     System.out.println("Enter your thing!");
-    BufferedReader bf = null;
     int num = 0;
-    try{
-      bf = new BufferedReader(new InputStreamReader(System.in));
+    try(BufferedReader bf = new BufferedReader(new InputStreamReader(System.in))){
       num = Integer.parseInt(bf.readLine());
-    }
-    finally{
-      bf.close();
     }
     System.out.println("Your num is " + num);
   };
