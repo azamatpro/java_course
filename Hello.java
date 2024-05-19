c792aeabf5b54825799f79731ca89d681cf23b3a
azamatpro <azamat.rasulov00@gmail.com>
2024-05-19 09:06:47 +0900

    Reading ints form client in two ways

--- Hello.java
+++ Hello.java
@@ -1,30 +1,17 @@
-class Hello{
-  public static void main(String args[]){
+import java.io.*;
+import java.util.Scanner;
 
-    // Primitive data types
-    // int num = 9;
-    // byte by = 127; // last byte number
-    // short sh = 55;
-    // long l = 7867l;
+public class Hello {
+  public static void main(String args[]) throws IOException{
+    System.out.println("Enter your thing!");
+    int num = 0;
 
-    // float f = 3.5f;
-    // double d = 3.5;
+    // InputStreamReader in = new InputStreamReader(System.in);
+    // BufferedReader bf = new BufferedReader(in); 
+    Scanner sc = new Scanner(System.in);
+    
+    num = sc.nextInt();
 
-    // char c = 'k';
-    // boolean b = true;
-
-    int a = 257;
-    byte b = 127;
-    byte k = (byte) a; // Casting => explicitly conversion
-    int s = b; // Conversion => Automatically converion
-
-    float f = 5.6f;
-    int nf = (int) f;
-
-    byte z = 10;
-    byte y = 30;
-    int res = z * y; // Type promotion
-
-    System.out.println(res);
-  }
-}
+    System.out.println(num);
+  };
+};
