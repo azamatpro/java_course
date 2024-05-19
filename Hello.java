976682273be1a491bc1524f1687359b5154e75ce
azamatpro <azamat.rasulov00@gmail.com>
2024-05-19 11:51:32 +0900

    Race condition, synchronized methods

--- Hello.java
+++ Hello.java
@@ -1,31 +1,40 @@
+class Counter {
+  int count;
+  public synchronized void increament(){
+    count++;
+  }
+}
 
 public class Hello {
   public static void main(String args[]){
+    Counter c = new Counter();
 
     Runnable objA = () -> {
-      for(int i = 1; i <= 5; i++){
-        System.out.println("Hi " + i);
-        try{
-          Thread.sleep(10);
-        }
-        catch(InterruptedException e){System.out.println(e);}
+      for(int i = 1; i <= 1000; i++){
+        c.increament();
       }
     };
 
     Runnable objB = () -> {
-      for(int i = 1; i <= 5; i++){
-        System.out.println("Hello " + i);
-        try{
-          Thread.sleep(10);
-        }
-        catch(InterruptedException e){System.out.println(e);}
+      for(int i = 1; i <= 1000; i++){
+        c.increament();
       }
     };
+
 
     Thread t1 = new Thread(objA);
     Thread t2 = new Thread(objB);
 
     t1.start();
     t2.start();
+
+    try{
+      // To force main() that wait for threads to complete their task
+      t1.join();
+      t2.join();
+    }catch(InterruptedException e){
+      System.out.println(e);  
+    }
+    System.out.println(c.count);
   };
 };
