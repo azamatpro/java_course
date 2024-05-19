6ba9d8fab58469f85183ea42e2e43b0b9f50b7c2
azamatpro <azamat.rasulov00@gmail.com>
2024-05-19 11:15:08 +0900

    Runnable vs Thread with Lamba expression

--- Hello.java
+++ Hello.java
@@ -1,39 +1,31 @@
-class A extends Thread{
-  public void run(){
-    for(int i = 1; i <= 10; i++){
-      System.out.println("Hi " + i);
-      try{
-        Thread.sleep(10);
-      }
-      catch(InterruptedException e){System.out.println(e);}
-    }
-  }
-}
-
-class B extends Thread{
-  public void run(){
-    for(int i = 1; i <= 10; i++){
-      System.out.println("Hello " + i);
-      try{
-        Thread.sleep(10);
-      }
-      catch(InterruptedException e){System.out.println(e);}
-    }
-  }
-}
 
 public class Hello {
   public static void main(String args[]){
-    A objA = new A();
-    B objB = new B();
 
-    objA.start();
-
-    try{
-        Thread.sleep(10);
+    Runnable objA = () -> {
+      for(int i = 1; i <= 5; i++){
+        System.out.println("Hi " + i);
+        try{
+          Thread.sleep(10);
+        }
+        catch(InterruptedException e){System.out.println(e);}
       }
-      catch(InterruptedException e){System.out.println(e);}
-      
-    objB.start();
+    };
+
+    Runnable objB = () -> {
+      for(int i = 1; i <= 5; i++){
+        System.out.println("Hello " + i);
+        try{
+          Thread.sleep(10);
+        }
+        catch(InterruptedException e){System.out.println(e);}
+      }
+    };
+
+    Thread t1 = new Thread(objA);
+    Thread t2 = new Thread(objB);
+
+    t1.start();
+    t2.start();
   };
 };
